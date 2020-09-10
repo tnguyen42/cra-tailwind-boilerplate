b3b5d08b2ece01374086563ccddd86a2c6b1a139
Thanh-Quy Nguyen <nguyen.tq@hotmail.fr>
2020-09-10 18:59:27 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+	return <div className="flex justify-center">Boilerplate with Tailwind</div>;
+}
+
+export default App;
---
+++ tailwind.js
@@ -0,0 +1,11 @@
+module.exports = {
+	purge: {
+		mode: "all",
+		content: ["./src/**/*.js", "./src/**/*.css"],
+	},
+	theme: {
+		extend: {},
+	},
+	variants: {},
+	plugins: [],
+};
